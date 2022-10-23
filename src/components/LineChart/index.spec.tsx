fa1d221047061c1c33a7fd14d662bc6f4d9c9a1b
Jonathan <jonathan.ibor@gmail.com>
2022-10-23 19:40:06 +0200

    fix(lint): linter

--- src/components/LineChart/index.spec.tsx
+++ src/components/LineChart/index.spec.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import {render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import LineChart from "./index";
 
-jest.mock('react-chartjs-2', () => ({
-	Line: () => null
+jest.mock("react-chartjs-2", () => ({
+  Line: () => null,
 }));
 
 describe("LineChart", () => {
-	it("should render LineChart", () => {
-		const { getByTestId } = render(<LineChart />);
-		
-		expect(getByTestId("line-chart")).toBeInTheDocument();
-	});
+  it("should render LineChart", () => {
+    const { getByTestId } = render(<LineChart />);
+
+    expect(getByTestId("line-chart")).toBeInTheDocument();
+  });
 });
